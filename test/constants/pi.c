d7c84ca2e1eabfdcfa7c80de2b566ae381a0954f
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2021-04-19 16:42:55 -0500

    Added proper test

--- test/constants/pi.c
+++
@@ -1,7 +0,0 @@
-#include "skipper/constants/pi.h"
-
-#include <stdio.h>
-
-int main(int argc, char* argv[]) {
-    printf("%f%s", sk_pi_f, "\n");
-}
---
+++ test/test.c
@@ -0,0 +1,5 @@
+#include "skipper/skipper.h"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
